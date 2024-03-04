941112d316cd882e42cd11cbb61cd91a3b70f3a6
asapawell <asapawell@gmail.com>
2024-03-04 18:01:21 +0300

    Добавление Аллюр отчета

--- src/test/java/AddItemToCartTest.java
+++ src/test/java/AddItemToCartTest.java
@@ -1,11 +1,16 @@
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.work.pages.ProductsPage;
 import ru.work.pages.YourCartPage;
+import ru.work.utils.Builder;
 
+import static io.qameta.allure.Allure.step;
 import static ru.work.utils.Builder.*;
 
+@Epic("Покупка товара")
 public class AddItemToCartTest extends BaseTest {
     ProductsPage productsPage = new ProductsPage();
     YourCartPage yourCartPage = new YourCartPage();
@@ -17,29 +22,37 @@ public class AddItemToCartTest extends BaseTest {
                 .setPassword("secret_sauce")
                 .clickLogin();
     }
-    //проверка добавления товара "Рюкзак" в корзину
-    @Test(priority = 1)
+
+    @Feature("Добавление товара в корзину")
+    @Test(priority = 1, description = "Проверка добавления товара \"Рюкзак\" в корзину")
     public void shouldAddItemToCart() {
-        productsPage
-                .addItemToCart(backpack)
-                .clickOnShoppingCart();
-        //проверка смены статуса у товара
-        Assert.assertEquals(productsPage.getChangedItemText(), "REMOVE");
-        //проверка счетчика у корзины
-        Assert.assertEquals(productsPage.getCountOfShoppingCartItems(), "1");
-        //проверка перехода в корзину
-        Assert.assertEquals(yourCartPage.getTitleText(), "Your Cart");
-        //проверка, что добавили нужный товар
-        Assert.assertEquals(yourCartPage.getAddedItems(), "Sauce Labs Backpack");
+        step("Добавить товар " + backpack.getName() + " в корзину", () -> {
+            productsPage
+                    .addItemToCart(backpack)
+                    .clickOnShoppingCart();
+        });
+        step("Проверяем, что у товара кнопка Add to Cart поменялась на Remove", () -> {
+            Assert.assertEquals(productsPage.getChangedItemText(), "REMOVE");
+        });
+        step("Проверяем, что у корзины появился счетчик и он равен количеству добавленных товаров (1)", () -> {
+            Assert.assertEquals(productsPage.getCountOfShoppingCartItems(), "1");
+        });
+        step("Проверяем переход в корзину", () -> {
+            Assert.assertEquals(yourCartPage.getTitleText(), "Your Cart");
+        });
+        step("Проверяем, что был добавлен товар с названием " + backpack.getName(), () -> {
+            Assert.assertEquals(yourCartPage.getAddedItems(), "Sauce Labs Backpack");
+        });
     }
+
     //Проверка для практики с DataProvider. Добавляем все оставшиеся товары по очереди.
-    @Test(priority = 2,dataProvider = "inputData")
-    public void shouldTestDataProvider(int index,String addedItem,String countOfAddedItems){
+    @Test(priority = 2, dataProvider = "inputData")
+    public void shouldTestDataProvider(int index, String addedItem, String countOfAddedItems) {
         productsPage
                 .addItemFromDataProvider(index)
                 .clickOnShoppingCart();
 
-        Assert.assertEquals(productsPage.getChangedItemTextFromDataProvider(index),"REMOVE");
+        Assert.assertEquals(productsPage.getChangedItemTextFromDataProvider(index), "REMOVE");
         Assert.assertEquals(productsPage.getCountOfShoppingCartItems(), countOfAddedItems);
         Assert.assertEquals(yourCartPage.getTitleText(), "Your Cart");
         Assert.assertEquals(yourCartPage.getAddedItemsFromDataProvider(index), addedItem);
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,19 +1,34 @@
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
 import ru.work.pages.ProductsPage;
 
+import static io.qameta.allure.Allure.step;
 import static org.testng.Assert.assertEquals;
 
+@Epic("Покупка товара")
 public class LoginTest extends BaseTest {
     ProductsPage productsPage = new ProductsPage();
+    @Feature("Авторизация")
+    @Story("Авторизация пользователя с главной страницы")
+    @Test(dataProvider = "inputData", description = "Проверка успешного входа в систему")
+    public void shouldLogin(String userName, String password) {
+        step("Устанавливаем логин " + userName + " " +
+                "и пароль " + password + " " + ", жмем на логин", () -> {
+            mainPage
+                    .setLogin(userName)
+                    .setPassword(password)
+                    .clickLogin();
+        });
 
-    @Test(dataProvider = "inputData")
-    public void shouldLogin(String userName,String password) {
-        mainPage
-                .setLogin(userName)
-                .setPassword(password)
-                .clickLogin();
         //Тесты, что после логина, мы попали на страницу с товарами.
-        assertEquals(productsPage.getSubTitle(), "Products");
-        assertEquals(productsPage.getCountOfItems(), 6);
+        step("Проверка успешного логина. Имеется заголовок Products", () -> {
+            assertEquals(productsPage.getSubTitle(), "Products");
+        });
+        step("Проверка, что при на странице Products, имеется 6 товаров", () -> {
+            assertEquals(productsPage.getCountOfItems(), 6);
+        });
+
     }
 }
